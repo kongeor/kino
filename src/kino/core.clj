b94ef2e1ca0f77a0504afa3003119c936fd7f6d7
Kostas Georgiadis <kongeor@gmail.com>
2019-08-02 09:17:11 +0300

    add nrepl server

--- src/kino/core.clj
+++ src/kino/core.clj
@@ -1,12 +1,16 @@
 (ns kino.core
   (:require [system.repl :refer [set-init! go]]
-            [kino.systems :refer [base-system]])
+            [kino.systems :refer [base-system]]
+            [environ.core :refer [env]]
+            [clojure.tools.nrepl.server :as serv])
   (:gen-class))
 
 (defn -main
   "Start a production system, unless a system is passed as argument (as in the dev-run task)."
   [& args]
   (let [system (or (first args) #'base-system)]
+    (if-let [nrepl-port (:nrepl-port env)]
+      (serv/start-server :port (Integer. nrepl-port)))
     (set-init! system)
     (go)))
 
