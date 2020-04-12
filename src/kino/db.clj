f22e940685588c6cf71ab8de8822fdeae4f58b41
Kostas Georgiadis <kongeor@gmail.com>
2020-04-12 11:44:12 +0300

    upgrade deps and use crux jdbc

--- src/kino/db.clj
+++ src/kino/db.clj
@@ -135,7 +135,7 @@
         existing (get-entity id)]
     (if existing
       existing
-      #_(do
+      (do
         (crux/submit-tx
           (-> system :db :db)
           [[:crux.tx/put
@@ -151,12 +151,13 @@
   (let [sys (-> system :db :db)]
     #_(spit "data.edn" (prn-str ))
 
-    #_(let [data (crux/tx-log sys (crux/new-tx-log-context sys) nil true)]
-      (clojure.pprint/pprint data (clojure.java.io/writer "data.edn")))
+    #_(doall
+      (let [data (crux/open-tx-log sys #_(crux/new-tx-log-context sys) nil true)]
+        (clojure.pprint/pprint (iterator-seq data) (clojure.java.io/writer "data.edn"))))
 
     #_(crux/attribute-stats sys)
 
-    (count
+    #_(count
       (entity-data (-> system :db :db)
         (crux/q (crux/db (-> system :db :db))
           '{:find [e]
@@ -173,7 +174,7 @@
       #inst "2020-05-18T09:20:27.966"
       #inst "2020-05-18T09:20:27.966")
 
-    #_(count
+    (count
       (crux/q
         (crux/db (-> system :db :db))
         '{:find [e]
@@ -195,9 +196,9 @@
       (for [tx (clojure.edn/read-string (slurp "data.edn"))]
         (let [tx' (:crux.api/tx-ops tx)
               tx'' (mapv (fn [[tx _ data]] [tx data]) tx')]
-          (when (seq tx'')
-            tx''
-            (crux/submit-tx sys tx'')))))
+          (when (seq tx')
+            tx'
+            (crux/submit-tx sys tx')))))
 
     #_(first (clojure.edn/read-string (slurp "daten.edn")))
 
